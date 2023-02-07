6ee7916843f91006048997962328d99f4ed70d3c
Abhilash4597 <abhilash4597@gmail.com>
2023-02-07 17:08:23 +0530

    player2 random dice value done

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const img1 = document.querySelector('.img1');
 const img2 = document.querySelector('.img2');
 
-const randomNum = Math.floor(Math.random() * 6) + 1;
-const randomImg = `dice${randomNum}.png`;
-const imgPath = `images/${randomImg}`;
+const randomNum1 = Math.floor(Math.random() * 6) + 1;
+const randomImg1 = `dice${randomNum1}.png`;
+const imgPath1 = `images/${randomImg1}`;
 
-img1.setAttribute('src', imgPath);
+img1.setAttribute('src', imgPath1);
+
+const randomNum2 = Math.floor(Math.random() * 6) + 1;
+const randomImg2 = `dice${randomNum2}.png`;
+const imgPath2 = `images/${randomImg2}`;
+
+img2.setAttribute('src', imgPath2);
