17e529336e0472dc94cbd4042cb35ebe2fcb72af
Abhilash4597 <abhilash4597@gmail.com>
2023-02-07 17:36:34 +0530

    winner and draw functionality add

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const img1 = document.querySelector('.img1');
 const img2 = document.querySelector('.img2');
+const heading = document.querySelector('.heading');
 
 const randomNum1 = Math.floor(Math.random() * 6) + 1;
 const randomImg1 = `dice${randomNum1}.png`;
@@ -8,7 +9,14 @@ const imgPath1 = `images/${randomImg1}`;
 img1.setAttribute('src', imgPath1);
 
 const randomNum2 = Math.floor(Math.random() * 6) + 1;
-const randomImg2 = `dice${randomNum2}.png`;
-const imgPath2 = `images/${randomImg2}`;
+const randomImgPath2 = `images/dice${randomNum2}.png`;
 
-img2.setAttribute('src', imgPath2);
+img2.setAttribute('src', randomImgPath2);
+
+if (randomNum1 > randomNum2) {
+  heading.innerHTML="Player-1 is Winner";
+} else if(randomNum1<randomNum2) {
+  heading.innerHTML="Player-2 is Winner";
+} else {
+  heading.innerHTML='Draw';
+}
